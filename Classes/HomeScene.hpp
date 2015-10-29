224888b74771254520068b013353ee9252c9a48a
Riya.Liel <s113111@edu.asahikawa-nct.ac.jp>
2015-10-29 17:00:18 +0900

    [Added]added some scenes

--- Classes/HomeScene.hpp
+++ Classes/HomeScene.hpp
@@ -10,6 +10,9 @@
 #define HomeScene_hpp
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "cocostudio/CocoStudio.h"
+#include "CatObject.hpp"
 
 class HomeScene : public cocos2d::Layer
 {
@@ -21,18 +24,37 @@ public:
     // implement the "static create()" method manually
     CREATE_FUNC(HomeScene);
     
+    bool onTouchBegin(cocos2d::Touch*,cocos2d::Event*);
+    void onTouchMoved(cocos2d::Touch*,cocos2d::Event*);
+    void onTouchEnded(cocos2d::Touch*,cocos2d::Event*);
+    void onTouchCancelled(cocos2d::Touch*,cocos2d::Event*);
+    
 protected:
+    enum class CAT_OBJECT{
+        TOY=0,
+        FUTON,
+        MEAL,
+        TRIMMER
+    };
+    
+    cocos2d::Node* home_scene;
+    cocos2d::Sprite* home_bg;
+    
+    //std::vector<std::unique_ptr<CatObject>> _cat_objects;
+    std::vector<cocos2d::ui::Button*> _cat_objects;
+    
     bool initLayout();
     bool initStatus();
     
     void comeCat();
+    void getCat(int id);
     
     void update(float dt);
     
     void saveScheduleTime();
     
     int _remain_event_time;
-    
+    cocos2d::Vec2 _old_pos;
     const int OCCURRE_EVENT_TIME = 500;
 };
 
